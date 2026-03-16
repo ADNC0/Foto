420c0740444261895681f86e46fdde4f917164e2
ADNC0 <abraham.nacimba5565@utc.edu.ec>
2026-03-15 19:37:09 -0500

    administrando el sitio para cada formulario

--- Aplicaciones/lente/admin.py
+++ Aplicaciones/lente/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario, SesionFotos, Foto, SeleccionCliente, FotoSeleccionada
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(SesionFotos)
+admin.site.register(Foto)
+admin.site.register(SeleccionCliente)
+admin.site.register(FotoSeleccionada)
